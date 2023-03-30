8bfaba04586073be97979a84f1ae989b302e37b6
Flaviu10 <flavius_flaviu9@yahoo.com>
2023-03-30 14:18:40 +0100

    add users get and post request and also block request for specify link

--- db.js
+++ db.js
@@ -8,9 +8,9 @@ const { Pool } = pg
 
 
 export const pool = new Pool({
-    user: process.env.USER,
-    host:  process.env.HOST,
-    database:  process.env.DATABASE,
-    password:  process.env.PASSWORD,
-    port:  process.env.PORT,
+    user: process.env.POSTGRESQL_ADDON_USER,
+    host:  process.env.POSTGRESQL_ADDON_HOST,
+    database:  process.env.POSTGRESQL_ADDON_DB,
+    password:  process.env.POSTGRESQL_ADDON_PASSWORD,
+    port:  process.env.POSTGRESQL_ADDON_PORT,
   });
